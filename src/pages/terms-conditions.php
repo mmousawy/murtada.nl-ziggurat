ee67e3c66cb83218440969f6d7c14b79d7043d6d
Murtada al Mousawy <murtada.al.mousawy@gmail.com>
2019-05-30 16:54:28 +0200

    Add terms and conditions page

---
+++ src/pages/terms-conditions.php
@@ -0,0 +1,102 @@
+<?php
+#zigg:title       = `Terms & Conditions`
+#zigg:page-title  = `Terms & Conditions`
+#zigg:slug        = `terms-conditions`
+#zigg:parent      = ``
+#zigg:cover-image = `_cover-terms-conditions{$size}.png`
+#zigg:date        = ``
+#zigg:description = `Read more about our terms and conditions.`
+?>
+
+<section class="page-section">
+  <div class="wrap wrap--terms-conditions">
+    <h2>Contents</h2>
+    <ol class="index">
+      <li><a href="terms-conditions#definitions">Definitions</a></li>
+      <li><a href="terms-conditions#quotation">Quotation</a></li>
+      <li><a href="terms-conditions#agreement">Agreement</a></li>
+      <li><a href="terms-conditions#pricing">Pricing</a></li>
+      <li><a href="terms-conditions#payment">Payment</a></li>
+      <li><a href="terms-conditions#delivery">Delivery</a></li>
+      <li><a href="terms-conditions#transfer-of-ownership">Transfer of ownership</a></li>
+      <li><a href="terms-conditions#ownership-means-of-production">Ownership means of production</a></li>
+      <li><a href="terms-conditions#client-ownership">Client ownership</a></li>
+      <li><a href="terms-conditions#intellectual-property">Intellectual property</a></li>
+    </ol>
+    <h2><a name="definitions">1. Definitions</a></h2>
+    <ol>
+      <li><u>Murtada webdesign bureau</u> has the following meanings: Murtada webdesign bureau including all its enterprises and successors, established in Spijkenisse.</li>
+      <li><u>Client</u> has the following meanings: the natural person or corporation or their successor who has given Murtada webdesign bureau the assignment to perform activities, or to issue a quotation.</li>
+      <li><u>Assignment</u> has the following meanings: the request of the client to Murtada webdesign bureau to produce one or multiple products or to perform activities.</li>
+      <li><u>Producing products</u> has the following meanings: to produce a physical product or a product present on a medium or on a so-called server.</li>
+      <li><u>Performing activities</u> has the following meanings: everything that Murtada webdesign bureau on behalf of the client produces or undertakes. This includes the development of concept and idea and also the composing of advice in the fields of marketing and communications.</li>
+      <li><u>Quotation</u> has the following meanings: to a greater or lesser extent specified production of products or performance of activities and the estimatated costs of these production of products or performance of activities.</li>
+      <li><u>Medium</u> has the following meanings: magnetic discs, optical discs and all other means for capturing, editing, sending, reproducing or publishing texts, images or other data, all this in the broadest sense of the term.</li>
+    </ol>
+    <h2><a name="quotation">2. Quotation</a></h2>
+    <ol>
+      <li>All quotations issued by Murtada webdesign bureau are an invitation to negotiate an offer. Murtada webdesign bureau always has the right to decline an assignment.</li>
+      <li>All quotations issued by Murtada webdesign bureau, unless otherwise agreed, are at the expense of the client. Murtada web design agency will determine the price for the quotation in advance and inform the client in writing.</li>
+      <li>Upon agreement that a quotation will be issued free or charge, particular conditions or arrangements may be stated. Failure to comply with these particular conditions or arrangements on the part of the client implies that the client is obliged actual expenses, including labor costs, which have been necessary to produce the quotation.</li>
+      <li>Quotations issued by Murtada webdesign bureau are based on information provided by the client. The client provides in good faith all the essential information required for the assignment.</li>
+      <li>When after a quotation the client wishes not to enter an agreement with Murtada webdesign bureau, all provided items in connection with the quotation have to be returned immediately.</li>
+    </ol>
+    <h2><a name="agreement">3. Agreement</a></h2>
+    <ol>
+      <li>An agreement with Murtada webdesign bureau will only be established after the written confirmation of the assignment has been signed for approval and returned.</li>
+      <li>The scope of the assignment will be determined by its description in the confirmation of the assignment of Murtada webdesign bureau, including all written amendments and additions that will be agreed upon in the future.</li>
+      <li>Changes in the assignment, after it has been issued, have to be notified promptly and in writing. Should changes be declared orally, then all possible adverse effects are at the expense of the client.</li>
+      <li>The changes in the assignment are effectual by, and at the time of acceptance by Murtada webdesign bureau.</li>
+      <li>Any possible increase or decrease of the quotation price due to changes in the assignment will be charged or credited to the client.</li>
+      <li>Changes in the assignment can result in the agreed upon time of delivery by Murtada webdesign bureau to be renounced.</li>
+    </ol>
+    <h2><a name="pricing">4. Pricing</a></h2>
+    <ol>
+      <li>The listed price in the agreement or quotation serves as a target. Murtada webdesign bureau reserves the right to charge up to 10% of the agreed price extra based on subsequent calculation. If by chance the new price exceeds the 10% limit, Murtada webdesign bureau will enter into negotiations as soon as possible to reach a new agreement. The client is obligated to re-enter negotiations with Murtada webdesign bureau. If no new agreement is reached, the client, in any case, has to reasonably compensate work done up to that time.  In any case, Murtada webdesign bureau retains the right to still perform the assignment at the original price, including the price after subsequent calculation.</li>
+      <li>Reasons for recalculation referred to in the preceding paragraph (article 4 paragraph 1) may be changes in prices before delivery has occurred for: raw materials, wages, monetary relations between the Dutch and foreign currencies, import duties, taxes and other factors affecting the cost, as well as more work performed that at the time of entering into the agreement was unforeseen by Murtada webdesign bureau.</li>
+      <li>The limitation in article 4 paragraph 1 of this article (maximum limit of 10% of original price) shall not apply if the client fails in their information requirements to Murtada webdesign bureau described in article 2 paragraph 4, or in the case when the client otherwise fails in their obligation to cooperate with Murtada webdesign bureau.</li>
+      <li>If no price is agreed upon between the two parties, and both parties have had one or several agreements prior to the agreement that were identical or virtually identical, the price will be calculated based on the corresponding methods and calculation rates of those agreements.</li>
+      <li>If outside of the application of the rule described in the preceding paragraph (article 4 paragraph 1) no agreement on the price has been reached, and if the price has only been an indicative target value, the price will be determined by what is considered to be a reasonable amount for the industry, or if that is not available, an amount that will cover a reasonable compensation (wage or salary) for the work performed.</li>
+    </ol>
+    <h2><a name="payment">5. Payment</a></h2>
+    <ol>
+      <li>Murtada webdesign bureau is responsible for timely billing. Partial invoicing is possible at any time.</li>
+      <li>Murtada webdesign bureau is entitled to demand a retainer (advanced payment) before or during the assignment with an invoice. Failure of payment thereof by the client, authorizes Murtada webdesign bureau to not commence work, to suspend or to terminate the agreement.</li>
+      <li>Invoices are due within 30 days after date of invoice and have to be paid in euros on an account designated by Murtada webdesign bureau.</li>
+      <li>In the event of a payment being overdue, the client is legally, even without a notice to that effect, indebted to a default interest from the due date, equal to the applicable statutory rate. The judicial and extra-judicial collection expenses are due as from the time that the client is in default and the claim has been referred to a third party for collection by Murtada webdesign bureau. Whereby the extrajudicial costs are fixed by both parties at 15% of the principal sum and current interest with a minimum of euro 112.50 excluding VAT.</li>
+      <li>Client must report any objections to the invoice within two weeks after the invoice date to Murtada webdesign bureau. The repost must be in writing and with reasons given for the objections. Failure in doing so, the client is deemed to have agreed to the amount of the invoice.</li>
+    </ol>
+    <h2><a name="delivery">6. Delivery</a></h2>
+    <ol>
+      <li>Delivery is made on the spot where Murtada web design agency and client have agreed to do so.</li>
+      <li>The delivery dates will be met by Murtada webdesign bureau's best effort. These delivery dates, however, are to be considered as an indication and never as a deadline. The mere fact that a stated delivery date is due does not bring Murtada webdesign bureau in default and gives the client no right to compensation, termination of the contract or non-fulfillment of any obligation towards Murtada webdesign bureau.</li>
+      <li>In the case of a significant exceedence of the delivery date is imminent, Murtada webdesign bureau will contact the client as soon as possible and consult to get to a mutually acceptable solution.</li>
+      <li>With compound assignments there is no obligation to supply a portion of the overall performance against the amount specified in the offer for this portion or against a proportionate part of the total price quoted, unless expressly agreed otherwise in writing.</li>
+    </ol>
+    <h2><a name="transfer-of-ownership">7. Transfer of ownership</a></h2>
+    <ol>
+      <li>Murtada webdesign bureau retains the ownership of all goods produced and results of work carried out until such time as all client debts to Murtada webdesign bureau have been payed off after delivery.</li>
+      <li>With the purchase of a website service or a content contract, a license will be granted for the use of those services or contents. For the use of mUpload, a license is granted and will never be property of the client and the client has no right to the source code.</li>
+    </ol>
+    <h2><a name="ownership-means-of-production">8. Ownership means of production</a></h2>
+    <ol>
+      <li>All by Murtada webdesign bureau manufactured goods such as: production means, semifinished products, tools and in particular: designs drawings, models, data carriers, computer software, data files, photographs and peripherals remain the property of Murtada webdesign bureau, even if they appear as separate items on the quotation, in the agreement or listed on the invoice.</li>
+      <li>Murtada webdesign bureau is not obliged to deliver the cases referred to in the preceding paragraph (article 8 paragraph 1) to the client.</li>
+      <li>Murtada webdesign bureau is not obliged to preserve or save the goods referred to in the first paragraph of this article (article 8 paragraph 1) for the client. If Murtada webdesign bureau and the client agree that these cases will be preserved by Murtada webdesign bureau, this shall not exceed the duration of one year and without Murtada webdesign bureau guaranteeing responsability for the suitability for repeated use.</li>
+    </ol>
+    <h2><a name="client-ownership">9. Client ownership</a></h2>
+    <ol>
+      <li>In the context of the fulfillment of the agreement, Murtada webdesign bureau will take custody of the client's affairs that have been handed over to them.</li>
+      <li>Notwithstanding the matters referred to in the preceding paragraph of this article (article 9 paragraph 1), the client bears all risks during custody of the matters meant in the preceding paragraph, unless a business insurance of Murtada webdesign bureau covers this risk. The client must choose to install an insurance policy for this risk. If desired, the client optionally chooses to sign an insurance policy for this risk with an insurance company theirselves.</li>
+      <li>The client is obliged, before handing over a copy, drawing, design, photographic recording or data carrier, to create a duplicate of these matters. The client shall preserve and store such goods in case during storage by Murtada webdesign bureau these matters are lost or unusable due to damage.</li>
+    </ol>
+    <h2><a name="intellectual-property">10. Intellectual property</a></h2>
+    <ol>
+      <li>Reports or documents that are provided in connection with consulting or research work, as well as manufactured goods by Murtada webdesign bureau fall within the scope of the Copyright Act 1912 (The Netherlands).</li>
+      <li>Intellectual property rights to the manufactured goods produced by Murtada webdesign bureau remain to Murtada webdesign bureau. The intellectual property is transferred only if expressly agreed in writing and on the condition that the client agreed fees within a reasonable time and in full.</li>
+      <li>Goods delivered or to be delivered by Murtada webdesign bureau according to their own design such as: copy, typesetting, design drawings, models, work and detail drawings, data carriers, computer software, data files, photographs, lithographs, films and similar production means, tools and devices, or a portion of the essence of the design belonging thereof may, whether or not there is copyright or other legal protection for Murtada webdesign bureau on them, not be reproduced within the framework of any production without written approval of Murtada webdesign bureau.</li>
+      <li>Reports or documents referred to in paragraph 1 of this article (article 10 paragraph 1) may be used only for internal use by the client, unless otherwise required by the nature of the agreement. For external use, such as publication or insight to non-employees, prior to the external use, the express written consent of Murtada web design agency is required.</li>
+      <li>Murtada webdesign bureau reserves the right for results obtained based on the assignment to be deployed for their own use or for third parties, for as long the interest of the client is not impaired.</li>
+    </ol>
+  </div>
+</div>
