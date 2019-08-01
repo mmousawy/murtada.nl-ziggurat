3ce2aface0fd11199a17eb5560742c1152ab8dbf
mmousawy <murtada.al.mousawy@gmail.com>
2019-08-01 17:44:08 +0200

    Fix issue with clicking twice on copy email button would trigger navigation

--- src/script/script.js
+++ src/script/script.js
@@ -207,6 +207,8 @@
   const emailCopy = e => {
     copyToClipboard(email);
 
+    e.preventDefault();
+
     if (document.querySelector('.tooltip')) {
       return;
     }
@@ -242,53 +244,52 @@
         newTooltip.remove();
       }, 250);
     }, 2000);
-
-    e.preventDefault()
   };
 
   function copyToClipboard (text) {
     // Use the Async Clipboard API when available. Requires a secure browing
     // context (i.e. HTTPS)
     if (navigator.clipboard) {
-      return navigator.clipboard.writeText(text)
+      return navigator.clipboard.writeText(text);
     }
 
     // ...Otherwise, use document.execCommand() fallback
 
     // Put the text to copy into a <span>
-    var span = document.createElement('span')
-    span.textContent = text
+    var span = document.createElement('span');
+    span.textContent = text;
 
     // Preserve consecutive spaces and newlines
-    span.style.whiteSpace = 'pre'
+    span.style.whiteSpace = 'pre';
 
     // Add the <span> to the page
     document.body.appendChild(span)
 
     // Make a selection object representing the range of text selected by the user
-    var selection = window.getSelection()
-    var range = window.document.createRange()
-    selection.removeAllRanges()
-    range.selectNode(span)
-    selection.addRange(range)
+    var selection = window.getSelection();
+    var range = window.document.createRange();
+    selection.removeAllRanges();
+    range.selectNode(span);
+    selection.addRange(range);
 
     // Copy text to the clipboard
     var success = false
+
     try {
       success = window.document.execCommand('copy')
     } catch (err) {
-      console.log('error', err)
+      console.log('error', err);
     }
 
     // Cleanup
-    selection.removeAllRanges()
-    window.document.body.removeChild(span)
+    selection.removeAllRanges();
+    window.document.body.removeChild(span);
 
     // The Async Clipboard API returns a promise that may reject with `undefined`
     // so we match that here for consistency.
     return success
       ? Promise.resolve()
-      : Promise.reject() // eslint-disable-line prefer-promise-reject-errors
+      : Promise.reject(); // eslint-disable-line prefer-promise-reject-errors
   }
 
   // Contact buttons
